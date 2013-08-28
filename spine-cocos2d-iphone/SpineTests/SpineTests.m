898a4eaa9bf8bbe45590a5bb0d4498ae839f0e56
Anders Borch <anders@cyborch.com>
2013-08-28 16:50:06 -0700

    Added static library target.

---
+++ spine-cocos2d-iphone/SpineTests/SpineTests.m
@@ -0,0 +1,34 @@
+//
+//  SpineTests.m
+//  SpineTests
+//
+//  Created by Anders Borch on 8/28/13.
+//  Copyright (c) 2013 Craig Hinrichs. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SpineTests : XCTestCase
+
+@end
+
+@implementation SpineTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
